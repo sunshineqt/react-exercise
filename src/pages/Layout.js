ac564ba6358c463dea91017821f38c57502c9a75
sunshineqt <17855847521@163.com>
2020-08-26 21:19:27 +0800

    组件复合时的类似具名slot与不具名slot

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -9,12 +9,27 @@ export default class Layout extends Component {
   }
   render() {
     const { children, showTopBar, showBottomBar } = this.props;
+    // const childrenArr = [];
+    // if (children.$$typeof) {
+    //   // 不具名
+    //   childrenArr.push(children);
+    // } else {
+    //   // 具名
+    //   for (let item in children) {
+    //     childrenArr.push(children[item]);
+    //   }
+    // }
     return (
       <div>
         {showTopBar && <TopBar />}
+        {/** {childrenArr.map((item, index) => {
+          return <React.Fragment key={index}>{item}</React.Fragment>;
+        })} */}
+
         {children.content}
         {children.txt}
         <button onClick={children.btnClick}>btn</button>
+
         {showBottomBar && <BottomBar />}
       </div>
     );
