031f70b00a4ecacf85e985fb15c855d73a3534b8
sunshineqt <17855847521@163.com>
2020-12-04 19:17:47 +0800

    test

--- src/pages/TreePage.js
+++ src/pages/TreePage.js
@@ -43,11 +43,33 @@ const treeData = {
 };
 
 export default class TreePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1,
+    };
+  }
+  componentDidMount() {
+    // 放入更新队列，统一执行，后面会覆盖前面的
+    this.setState({ count: this.state.count + 1 }, () => {
+      console.log(this.state.count, "执行第1次回调"); // 4
+    });
+    console.log(this.state.count, "执行第1次"); //1
+    this.setState({ count: this.state.count + 2 }, () => {
+      console.log(this.state.count, "执行第2次回调"); //4
+    });
+    console.log(this.state.count, "执行第2次"); //1
+    this.setState({ count: this.state.count + 3 }, () => {
+      console.log(this.state.count, "执行第3次回调"); //4
+    });
+    console.log(this.state.count, "执行第3次"); //1
+  }
   render() {
     return (
       <div>
         <h1>treepage</h1>
         <TreeNode data={treeData} />
+        {this.state.count}
       </div>
     );
   }
